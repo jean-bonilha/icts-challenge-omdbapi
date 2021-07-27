cd27f1e9b5b5d43149362d39c9d772fa99f4aa26
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2021-07-27 05:16:15 -0400

    develop(C4):clear unneeded code

--- src/app/dashboard/Dashboard.js
+++ src/app/dashboard/Dashboard.js
@@ -1,48 +1,7 @@
 import React, { Component } from 'react';
-import { Doughnut } from 'react-chartjs-2';
 import Slider from "react-slick";
-import { TodoListComponent } from '../apps/TodoList'
-import { VectorMap } from "react-jvectormap"
-
-const mapData = {
-  "BZ": 75.00,
-  "US": 56.25,
-  "AU": 15.45,
-  "GB": 25.00,
-  "RO": 10.25,
-  "GE": 33.25
-}
 
 export class Dashboard extends Component {
-
-  transactionHistoryData =  {
-    labels: ["Paypal", "Stripe","Cash"],
-    datasets: [{
-        data: [55, 25, 20],
-        backgroundColor: [
-          "#111111","#00d25b","#ffab00"
-        ]
-      }
-    ]
-  };
-
-  transactionHistoryOptions = {
-    responsive: true,
-    maintainAspectRatio: true,
-    segmentShowStroke: false,
-    cutoutPercentage: 70,
-    elements: {
-      arc: {
-          borderWidth: 0
-      }
-    },      
-    legend: {
-      display: false
-    },
-    tooltips: {
-      enabled: true
-    }
-  }
 
   sliderSettings = {
     infinite: true,
@@ -50,9 +9,7 @@ export class Dashboard extends Component {
     slidesToShow: 1,
     slidesToScroll: 1
   }
-  toggleProBanner() {
-    document.querySelector('.proBanner').classList.toggle("hide");
-  }
+
   render () {
     return (
       <div>
