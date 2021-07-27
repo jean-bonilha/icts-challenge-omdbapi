c02181a00ad601fd60448c808735fb16ed024d12
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2021-07-27 05:07:52 -0400

    develop(C2):create a static layout for films search

--- src/app/App.js
+++ src/app/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './App.scss';
 import AppRoutes from './AppRoutes';
-import Navbar from './shared/Navbar';
-import Sidebar from './shared/Sidebar';
-import Footer from './shared/Footer';
 import { withTranslation } from "react-i18next";
 
 class App extends Component {
@@ -13,19 +10,13 @@ class App extends Component {
     this.onRouteChanged();
   }
   render () {
-    let navbarComponent = !this.state.isFullPageLayout ? <Navbar/> : '';
-    let sidebarComponent = !this.state.isFullPageLayout ? <Sidebar/> : '';
-    let footerComponent = !this.state.isFullPageLayout ? <Footer/> : '';
     return (
       <div className="container-scroller">
-        { sidebarComponent }
         <div className="container-fluid page-body-wrapper">
-          { navbarComponent }
           <div className="main-panel">
             <div className="content-wrapper">
               <AppRoutes/>
             </div>
-            { footerComponent }
           </div>
         </div>
       </div>
@@ -39,7 +30,6 @@ class App extends Component {
   }
 
   onRouteChanged() {
-    console.log("ROUTE CHANGED");
     const { i18n } = this.props;
     const body = document.querySelector('body');
     if(this.props.location.pathname === '/layout/RtlLayout') {
