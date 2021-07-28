35aca191adfc8978dbd54edd7500ee5c162629bc
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2021-07-28 04:33:35 -0400

    develop(C8):add Provider to App component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import "./i18n";
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { store } from './redux';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 , document.getElementById('root'));
 
